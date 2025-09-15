fc55689882de7825594c10cc4e84a1365ddd5262
jrogoz <joanna.rogoz33@gmail.com>
2025-09-15 14:15:59 +0200

    Create Task model

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from sqlalchemy import create_engine, Column, Integer, String, Enum
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+
+import enum
 
 app = FastAPI()
+DATABASE_URL = "Sqlite:///./test.db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocomit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class StatusEnum(enum.Enum):
+    TODO = "todo"
+    IN_PROGRESS = "in_progress"
+    DONE = "done"
+    CANCELLED = "cancelled"
+    BLOCKED = "blocked"
+    
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=False)
+    status = Column(Enum(StatusEnum), nullable=False)
