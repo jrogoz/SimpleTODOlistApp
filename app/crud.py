66e72794524d632e33bda9ee7a7dbda48b7bb532
jrogoz <joanna.rogoz33@gmail.com>
2025-09-16 09:07:07 +0200

    add read endpoints amd cruds

--- app/crud.py
+++ app/crud.py
@@ -7,3 +7,9 @@ def create_task(db: Session, task: schemas.TaskCreate):
     db.commit()
     db.refresh(db_task)
     return db_task
+
+def get_tasks(db: Session):
+    return db.query(models.Task).all()
+
+def get_task(db: Session, task_id: int):
+    return db.query(models.Task).filter(models.Task.id==task_id).first()
--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -9,14 +9,13 @@ router = APIRouter()
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     return crud.create_task(db=db, task=task)
 
+@router.get("/", response_model=list[schemas.TaskRead])
+def read_tasks(db: Session = Depends(get_db)):
+    return crud.get_tasks(db=db)
 
-# @router.get("/tasks/")
-# def read_tasks(db: Session = Depends(get_db)):
-#     return db.query(Task).all()
-
-# @router.get("/tasks/{task_id}")
-# def read_task(task_id: int, db: Session = Depends(get_db)):
-#     task = db.query(Task).filter(Task.id == task_id).first()
-#     if not task:
-#         raise HTTPException(status_code=404, detail="Task not found")
-#     return task
+@router.get("/{task_id}", response_model=schemas.TaskRead)
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    task = crud.get_task(db=db, task_id=task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
