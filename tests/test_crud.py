11e1b6cbc0a6f93314b30d8207d6dd36b6e995de
jrogoz <joanna.rogoz33@gmail.com>
2025-09-17 15:14:03 +0200

    add get all tasks crud tests

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -33,8 +33,34 @@ def test_get_task(db_session):
     assert task_db.status == task.status
     assert task_db.title == task.title
 
-
 def test_get_task_invalid_task_id(db_session):
     task_db = crud.get_task(db=db_session, task_id=1234)
 
     assert task_db is None
+
+def test_get_all_tasks(db_session):
+
+    tasks = [
+        create_simple_task(db_session),
+        create_simple_task(db_session)
+    ]
+    
+
+    all_tasks_db = crud.get_tasks(db=db_session)
+
+    assert isinstance(all_tasks_db, list)
+    assert len(all_tasks_db) == 2
+
+    for t, t_db in zip(tasks, all_tasks_db):
+        assert isinstance(t_db, object)
+
+        assert t.id == t_db.id
+        assert t.title == t_db.title
+        assert t.status == t_db.status
+
+def test_get_all_tasks_no_tasks(db_session):
+    all_tasks_db = crud.get_tasks(db=db_session)
+
+    assert all_tasks_db is not None
+    assert all_tasks_db == []
+
