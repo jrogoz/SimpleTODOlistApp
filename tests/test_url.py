5b00fc48f83a6aa31fc10d70db4c40fafb74b39a
jrogoz <joanna.rogoz33@gmail.com>
2025-09-18 15:40:45 +0200

    add get task url tests

--- tests/test_url.py
+++ tests/test_url.py
@@ -4,13 +4,16 @@ from app.routers.tasks import create_task
 from app.schemas import TaskCreate
 from app.models import StatusEnum
 
-def test_create_task(client):
+def create_simple_task(client):
     task = TaskCreate(title = 'New task')
-
     response = client.post(
         '/tasks/',
         json=jsonable_encoder(task)
     )
+    return task, response
+
+def test_create_task(client):
+    task, response = create_simple_task(client)
 
     assert response is not None
     assert response.status_code == 201
@@ -25,5 +28,36 @@ def test_create_task(client):
     assert StatusEnum(task_url['status']) == StatusEnum.TODO
 
     assert task_url['title'] is not None
-    assert (task_url['title'], str)
+    assert isinstance(task_url['title'], str)
     assert task_url['title'] == task.title
+
+def test_get_task(client):
+    _, response = create_simple_task(client)
+
+    task = response.json()
+
+    response = client.get(f'/tasks/{task['id']}')
+
+    assert response is not None
+    assert response.status_code == 200
+
+    task_url = response.json()
+
+    assert task_url['id'] is not None
+    assert isinstance(task_url['id'], int)
+    assert task_url['id'] == task_url['id']
+
+    assert task_url['title'] is not None
+    assert isinstance(task_url['title'], str)
+    assert task_url['title'] == task_url['title']
+
+    assert task_url['status'] is not None
+    assert isinstance(StatusEnum(task_url['status']), StatusEnum)
+    assert task_url['status'] == task_url['status']
+
+def test_get_task_invalid_id(client):
+    response = client.get('/tasks/12345')
+
+    assert response is not None
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Task not found'}
